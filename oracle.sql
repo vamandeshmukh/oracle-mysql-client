70304eb266d561a62b7f134ac483e3eb835ebdea
Vaman Deshmukh <vamandeshmukh@gmail.com>
2022-08-24 14:30:15 +0530

    python

--- oracle.sql
+++ oracle.sql
@@ -36,5 +36,39 @@ SELECT * FROM city WHERE city LIKE 'a%' ORDER BY city_id;
 
 SELECT * FROM vaman;
 
+show errors;
 
+SELECT * FROM city WHERE city = 'vaman';
+
+GET DIAGNOSTICS @p1 = NUMBER, @p2 = ROW_COUNT;
+
+DROP TABLE test.no_such_table;
+
+GET DIAGNOSTICS CONDITION 1 @p1 = RETURNED_SQLSTATE, @p2 = MESSAGE_TEXT, @P3 = MYSQL_ERRNO;
+
+
+
+
+
+SELECT COUNT(*) FROM film;
+
+SELECT COUNT(*) FROM film_text;
+
+-- SELECT * FROM city WHERE city LIKE ORDER BY city_id LIMIT;
+
+SELECT * FROM city WHERE city LIKE 'a%' ORDER BY city_id;
+
+
+
+SELECT * FROM emp ORDER BY eid;
+
+CREATE OR REPLACE VIEW emps_with_high_salary AS SELECT * FROM emp WHERE salary > 95000 ORDER BY salary; 
+
+SELECT * FROM emps_with_high_salary;
+
+DELETE FROM emp WHERE ename = 'Aliya';
+
+INSERT INTO emp VALUES (116, 'Abcd', 95500);
+
+SET SQL_SAFE_UPDATES = 0;
 
